c7af186688b49c22a0eeb095fd569ee9eeb02d26
Pedro Santiago <pedro@i10digital.com.br>
2021-10-29 15:32:53 -0300

    Update rbac.php

--- config/rbac.php
+++ config/rbac.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Policies\RolePolicy;
-use App\Policies\UserPolicy;
-use App\Providers\NovaServiceProvider;
-
-
 return [
     /*
     |--------------------------------------------------------------------------
